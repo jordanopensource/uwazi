fb29775048e47e8b2b7691e5b98370bbd36796e9
Santiago <71732018+Zasa-san@users.noreply.github.com>
2024-11-18 11:41:57 -0300

    mimic blank-state's behavior (#7447)

--- database/admin_user.sh
+++ database/admin_user.sh
@@ -3,9 +3,9 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-DB=${1:-uwazi_development}
-HOST=${2:-${DBHOST:-127.0.0.1}}
+DB=${filtered[0]:-${DATABASE_NAME:-uwazi_development}}
+HOST=${filtered[1]:-${DBHOST:-127.0.0.1}}
 
 echo -e "\n\nDeleting $DB database on $HOST"
-mongosh --quiet -host $HOST $DB --eval "db.users.drop()"
-mongorestore -h $HOST blank_state/admin_user/ --db=$DB
+mongosh --quiet -host "$HOST" "$DB" --eval "db.users.drop()"
+mongorestore -h "$HOST" blank_state/admin_user/ --db="$DB"
